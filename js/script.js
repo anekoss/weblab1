0c3e4d0828812334436bf1f6e6fdbe359daf0165
anekoss <ann1974chernova@gmail.com>
2022-09-26 19:18:39 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,110 @@
+let x;
+let y;
+let r;
+function checkX() {
+    if (!validateRadio("X")) {
+        showMessage("Значение X не выбрано")
+        return false;
+    }
+    x = getRadio("X").eq(0).val();
+    return true;
+}
+
+function checkY() {
+    let yField = $('#y-text');
+    let yVal = yField.val().replace(',', '.');
+    if (yVal === "") {
+        showMessage("Значение Y не введено")
+        return false;
+    } else if (yVal.match(/^[+-]0$/) || yVal.match(/^[+-]0\.0+$/) ||
+         yVal.match(/^[+-]?00+$/) || yVal.match(/^[+-]?00+\.[0-9]+$/) ||
+        !yVal.match(/^-?[0-9]+\.[0-9]+$/) && !yVal.match(/^-?[0-9]+$/)) {
+        showMessage("Значение Y должно быть числом");
+        return false;
+    }
+    if (yVal <= -3 || yVal >= 3) {
+        showMessage("Значение Y не входит в интервал (-3,3)");
+        return false;
+    }
+    y = yVal;
+    return true;
+}
+
+function checkR() {
+    if (!validateRadio("R")) {
+        showMessage("Значение R не выбрано")
+        return false;
+    }
+    r = getRadio("R").eq(0).val();
+    return true;
+}
+
+function validateRadio(name) {
+    return getRadio(name).length === 1;
+}
+
+function getRadio(name) {
+    return $(`:input[name = "${name}"]:checked`);
+}
+
+function checkForm() {
+    let xValid = checkX();
+    let yValid = checkY();
+    let rValid = checkR();
+    return xValid && yValid && rValid;
+}
+
+function showMessage(message) {
+    let errorMessage = document.createElement("div");
+    errorMessage.textContent = message;
+    $('#errors').append(errorMessage);
+}
+
+function clearError() {
+    $('#errors').empty();
+}
+
+$('#reset').click(function () {
+    $('#errors').empty();
+})
+
+function clearTable() {
+    let table = "<tr>\n" +
+        "        <th>X</th>\n" +
+        "        <th>Y</th>\n" +
+        "        <th>R</th>\n" +
+        "        <th>Текущее время</th>\n" +
+        "        <th>Время работы скрипта</th>\n" +
+        "        <th>Результат</th>\n" +
+        "    </tr>"
+    $('#result-table').html(table);
+}
+
+$('#main-form').on('submit', function (event) {
+    event.preventDefault();
+    clearError();
+    if (checkForm()) {
+        $.ajax({
+            url: "php/main.php",
+            type: 'POST',
+            dataType: 'json',
+            data: "X=" + x + "&Y=" + y + "&R=" + r,
+            success: function (jsonData) {
+                if (jsonData.error) {
+                    clearError();
+                    showMessage("Получены неверные данные");
+                    return;
+                }
+                let row = '<tr>';
+                row += '<td>' + jsonData.x + '</td>';
+                row += '<td>' + jsonData.y + '</td>';
+                row += '<td>' + jsonData.r + '</td>';
+                row += '<td>' + jsonData.current + '</td>';
+                row += '<td>' + jsonData.execution + '</td>';
+                row += '<td>' + jsonData.result + '</td>';
+                row += '</tr>';
+                $('#result-table').append(row);
+            },
+        });
+    }
+})
---
+++ php/main.php
@@ -0,0 +1,58 @@
+<?php
+
+function writeLogs($message)
+{
+    $logs = fopen('php.log', 'a+');
+    fwrite($logs, date('d.m.y H:i:s') . ': ' . $message . "\n");
+    fclose($logs);
+}
+
+function validate() {
+    return isset($_POST['X']) && isset($_POST['Y']) && isset($_POST['R']);
+}
+
+function checkTriangle($x, $y, $r) {
+    return $x <= 0 && $y <= 0 && $y >= - $x - $r;
+}
+
+function checkRectangle($x, $y, $r) {
+    return $x <= 0 && $x >= -$r && $y >= 0 && $y <= $r;
+}
+function checkSector ($x, $y, $r) {
+    return $x >= 0 && $y >= 0 && sqrt($x * $x + $y * $y) <= $r * $r;
+}
+
+function checkArea($x, $y, $r) {
+    return checkTriangle($x, $y, $r) || checkRectangle($x, $y, $r) || checkSector($x, $y, $r);
+}
+
+function validateX($y) {
+    return is_numeric($y) && ($y == -4 || $y == -3 || $y == -2 || $y == -1 || $y == 0 || $y == 1 || $y == 2 || $y == 3 || $y == 4);
+}
+
+function validateY($x) {
+    return is_numeric($x) && $x > -3 && $x <= 3;
+}
+
+function validateR($r) {
+    return is_numeric($r) && ($r == 1 || $r == 1.5 || $r == 2 || $r == 2.5 || $r == 3);
+}
+
+if (validate()) {
+    date_default_timezone_set('Europe/Moscow');
+    $x = trim($_POST['X']);
+    $y = trim($_POST['Y']);
+    $r = trim($_POST['R']);
+    if (validateX($x) && validateY($y) && validateR($r)) {
+        $currentTime = date("d.m.Y H:i:s", time());
+        $scriptTime = round(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 6);
+        $inArea = checkArea($x, $y, $r) ? 'Да':'Нет';
+        $data =  "{\"x\" : $x, \"y\" : $y, \"r\" : $r, \"current\" : \"$currentTime\", \"execution\" : $scriptTime, \"result\" : \"$inArea\"}";
+        echo $data;
+    } else echo "{\"error\" : true}";
+} else {
+    echo "error";
+}
+
+
+
